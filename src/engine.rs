01aba6b413696c6b327db82695b654fe703478a5
Carlos Carral C <carloscarral13@gmail.com>
2022-06-13 05:26:09 -0500

    No baja vida cuando la posicion es la misma que la pasada

--- src/engine.rs
+++ src/engine.rs
@@ -1,12 +1,10 @@
-use crate::error::EngineError;
-use console_error_panic_hook;
+use crate::error::{EngineError, JsEngineError};
 use interprete_tanques::tank_status::{Position, TankStatus};
 use interprete_tanques::Interpreter;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub struct TanquesEngine {
-    interpretes: Vec<Interpreter<'static>>,
+pub struct TanquesEngine<'a> {
+    interpretes: Vec<Interpreter<'a>>,
 }
 
 #[derive(Clone)]
@@ -29,24 +27,8 @@ impl GameStatus {
         }
     }
 
-    #[wasm_bindgen(getter)]
-    pub fn get_tanks_status_0(&self) -> TankStatus {
-        self.tanks_status[0]
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn get_tanks_status_1(&self) -> TankStatus {
-        self.tanks_status[1]
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn get_tanks_status_2(&self) -> TankStatus {
-        self.tanks_status[2]
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn get_tanks_status_3(&self) -> TankStatus {
-        self.tanks_status[3]
+    pub fn get_tank_status(&self, idx: usize) -> TankStatus {
+        self.tanks_status[idx]
     }
 }
 
@@ -56,11 +38,11 @@ impl Default for GameStatus {
     }
 }
 
-impl TanquesEngine {
+impl<'a> TanquesEngine<'a> {
     pub const SHOT_DAMAGE: usize = 20;
     pub const WALL_COLLISION_DAMAGE: usize = 15;
     pub const DEFAULT_COLLISION_DAMAGE: usize = 15;
-    pub fn new(progs: Vec<&'static str>) -> Result<Self, EngineError> {
+    pub fn new(progs: Vec<&'a str>) -> Result<Self, EngineError> {
         let interpretes: Vec<Interpreter> = progs
             .iter()
             .enumerate()
@@ -80,6 +62,7 @@ impl TanquesEngine {
             .zip(new_game_status.tanks_status.iter_mut())
             .map(|((idx, interprete), status)| {
                 // Clear last shot status
+                status.set_got_shot(false);
                 status.set_shot(false);
                 interprete
                     .step_inst(status)
@@ -92,32 +75,32 @@ impl TanquesEngine {
         let shot_damage_list: Vec<Option<Vec<usize>>> = new_status
             .iter()
             .enumerate()
-            .map(|(idx, ts)| -> Option<Vec<usize>> {
-                if ts.shot() {
+            .map(|(idx, current_tank_status)| -> Option<Vec<usize>> {
+                if current_tank_status.shot() {
                     // Get the tank's direction
-                    let (my_x, my_y) = ts.get_pos();
+                    let (my_i, my_j) = current_tank_status.get_pos();
                     // Get a list of the tanks it actually hit
                     let mut tanks_hit = vec![];
-                    for i in 0..4 {
+                    for k in 0..4 {
                         // Skip over self
-                        if idx != i {
-                            let (other_x, other_y) = new_status[i].get_pos();
-                            let hit = match ts.get_dir() {
-                                interprete_tanques::tank_status::TankDirection::North => {
-                                    my_y > other_y && my_x == other_x
-                                }
+                        if idx != k {
+                            let (other_i, other_j) = new_status[k].get_pos();
+                            let hit = match current_tank_status.get_dir() {
                                 interprete_tanques::tank_status::TankDirection::West => {
-                                    my_x > other_x && my_y == other_y
+                                    my_j > other_j && my_i == other_i
                                 }
-                                interprete_tanques::tank_status::TankDirection::South => {
-                                    my_y < other_y && my_x == other_x
+                                interprete_tanques::tank_status::TankDirection::North => {
+                                    my_i > other_i && my_j == other_j
                                 }
                                 interprete_tanques::tank_status::TankDirection::East => {
-                                    my_x < other_x && my_y == other_y
+                                    my_j < other_j && my_i == other_i
+                                }
+                                interprete_tanques::tank_status::TankDirection::South => {
+                                    my_i < other_i && my_j == other_j
                                 }
                             };
                             if hit {
-                                tanks_hit.push(i);
+                                tanks_hit.push(k);
                             }
                         }
                     }
@@ -135,21 +118,22 @@ impl TanquesEngine {
                 Some(damaged_tanks) => {
                     for i in damaged_tanks {
                         new_status[*i].apply_damage(TanquesEngine::SHOT_DAMAGE);
+                        new_status[*i].set_got_shot(true);
                     }
                 }
                 None => {}
             });
 
         // Apply damage from collisions with walls
-        game_status
-            .tanks_status
-            .iter()
-            .zip(new_status.iter_mut())
-            .for_each(|(old, new)| {
-                if old.get_pos() == new.get_pos() {
-                    new.apply_damage(TanquesEngine::WALL_COLLISION_DAMAGE);
-                }
-            });
+        // game_status
+        // .tanks_status
+        // .iter()
+        // .zip(new_status.iter_mut())
+        // .for_each(|(old, new)| {
+        // if old.get_pos() == new.get_pos() {
+        // new.apply_damage(TanquesEngine::WALL_COLLISION_DAMAGE);
+        // }
+        // });
 
         // Apply damage from collisions with other tanks
         let collision_other_tanks: Vec<bool> = new_status
@@ -184,40 +168,59 @@ impl TanquesEngine {
 
 #[wasm_bindgen]
 pub struct EngineApi {
-    inner_engine: TanquesEngine,
+    step: usize,
+    programs: Vec<String>,
+    first_status_storage: Option<GameStatus>,
 }
 
 #[wasm_bindgen]
 pub struct ApiInitWrapper {
-    programs: Vec<&'static str>,
+    programs: Vec<String>,
 }
 
 #[wasm_bindgen]
+#[allow(clippy::new_without_default)]
 impl ApiInitWrapper {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
         Self {
-            programs: vec![
-                "var x = 4; mientras ( x > 0 ){ si( radar == 0 ){ gira derecha; }otro{ avanza; }}",
-                "var y = 10;",
-                "var z = 3;",
-                "var w = 0;",
-            ],
+            programs: vec![String::default(); 4],
         }
+    }
+
+    pub fn set_prog(&mut self, prog: String, idx: usize) {
+        self.programs[idx] = prog;
     }
 }
 
 #[wasm_bindgen]
 impl EngineApi {
     #[wasm_bindgen(constructor)]
-    pub fn new(progs: ApiInitWrapper) -> Result<EngineApi, String> {
+    pub fn new(progs: ApiInitWrapper) -> Result<EngineApi, JsEngineError> {
         let progs = progs.programs;
-        let inner_engine = TanquesEngine::new(progs).map_err(|e| format!("{e}"))?;
-        Ok(EngineApi { inner_engine })
+        let v: Vec<&str> = progs.iter().map(|s| &**s).collect();
+        TanquesEngine::new(v).map_err(JsEngineError::from_engine_error)?;
+        Ok(EngineApi {
+            step: 0,
+            programs: progs,
+            first_status_storage: None,
+        })
     }
 
     pub fn step(&mut self, status: &GameStatus) -> Result<GameStatus, String> {
-        let status = self.inner_engine.step(status).map_err(|e| format!("{e}"))?;
+        let progs = self.programs.clone();
+        let v: Vec<&str> = progs.iter().map(|s| &**s).collect();
+        let mut inner_engine = TanquesEngine::new(v).map_err(|e| format!("{e}"))?;
+
+        if self.step == 0 {
+            self.first_status_storage = Some(status.clone());
+        }
+
+        let mut status = self.first_status_storage.as_ref().unwrap().to_owned();
+        self.step += 1;
+        for _i in 0..self.step {
+            status = inner_engine.step(&status).map_err(|e| format!("{e}"))?;
+        }
         Ok(status)
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,33 @@
 use interprete_tanques::error::ErrorInterprete;
 use interprete_tanques::LineColLocation;
 use std::error::Error;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[derive(Debug)]
 pub enum EngineError {
     InitError(usize, LineColLocation),
     RuntimeError(usize, ErrorInterprete),
+}
+
+#[derive(Debug)]
+#[wasm_bindgen]
+pub struct JsEngineError {
+    pub engine_idx: usize,
+    pub line: usize,
+}
+
+impl JsEngineError {
+    pub fn from_engine_error(e: EngineError) -> Self {
+        match e {
+            EngineError::InitError(engine_idx, LineColLocation::Pos((line, _col))) => {
+                JsEngineError { engine_idx, line }
+            }
+            EngineError::InitError(_engine_idx, LineColLocation::Span(_, _)) => {
+                panic!("Error de SPAN (linea 27 error.rs)");
+            }
+            EngineError::RuntimeError(_engine_idx, _) => todo!(),
+        }
+    }
 }
 
 impl std::fmt::Display for EngineError {
@@ -15,9 +37,18 @@ impl std::fmt::Display for EngineError {
                 f.write_fmt(format_args!("Error  en el interprete {i} en {pos:#?}"))
             }
             EngineError::RuntimeError(i, e) => f.write_fmt(format_args!(
-                "Error en el ejecución de interprete {i}:  en {e}"
+                "Error en el ejecución de internprete {i}:  en {e}"
             )),
         }
+    }
+}
+
+impl std::fmt::Display for JsEngineError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "Error en el interprete {} en linea {}",
+            self.engine_idx, self.line
+        ))
     }
 }
 
